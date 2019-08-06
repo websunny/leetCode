6ef3c8f14383018d02bde26ea1dfca9ca7b14dca
舒怡聪 <shuyicong@vpgame.cn>
2019-08-06 10:39:53 +0800

    feat:leetCode 1&&2

---
+++ 1.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var dict = {}
+    var len =nums.length
+    var i,j
+    for (i=0;i<len;i++){
+      dict[nums[i]]=i
+    }
+    for(j=0;j<len;j++){
+      if(dict[target-nums[j]] && dict[target-nums[j]]!==j){
+        return [dict[target-nums[j]],j]
+      }
+    }
+};
+
+console.log(twoSum([2,7,11,15],9))
---
+++ 2.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    function xun(ll){
+        var lists = [];
+        while(ll!==null){
+            lists.push(ll.val);
+            ll=ll.next;
+        }
+        return lists;
+    }
+    var ll1=xun(l1);
+    var ll2=xun(l2);
+    
+    let len=ll1.length
+    if(len<ll2.length){
+      len=ll2.length
+    }
+    let res=[0]
+    for(let i = 0; i<len;i++){
+      if(!ll1[i]){
+        ll1[i]=0
+      }
+      if(!ll2[i]){
+        ll2[i]=0
+      }
+      if(res[i]+ll1[i]+ll2[i]>9){
+        res[i]=res[i]+ll1[i]+ll2[i]-10
+        res[i+1]=1
+      }else{
+        res[i]=res[i]+ll1[i]+ll2[i]
+        res[i+1]=0
+      }
+    }
+    if(!res[res.length-1]){
+      res.splice(-1,1)
+    }
+    function array2list(ary) {
+    if(ary.length === 0) {
+        return null
+    }
+    var nodes = []
+    
+    for(var i = 0; i < ary.length; i++) {
+        var node = {}
+        node.val = ary[i]
+        node.next = null
+        nodes.push(node)
+    }
+
+    for(var i = 0; i < nodes.length - 1; i++) {
+        nodes[i].next = nodes[i + 1]
+    }
+
+    return nodes[0]
+}
+    return array2list(res)
+};
